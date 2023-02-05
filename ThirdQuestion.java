6aaee9845460884f0c868fec56deacd34b450cf5
Supun Wijesooriya <81010139+supunxiii@users.noreply.github.com>
2023-02-05 16:11:08 +0530

    Add files via upload

---
+++ ThirdQuestion.java
@@ -0,0 +1,11 @@
+public class ThirdQuestion{
+  public static void main(String[] args){
+    String animal1 = "quick brown fox";
+    String animal2 = "lazy dog";
+    String article = "the";
+    String action = "jumps over";
+    String sentence = article + " " + animal1 + " " + action + " " + article + " " + animal2;
+    System.out.println(sentence);
+    System.out.println("Total length of the sentence is "+ sentence.length());
+ }
+}
